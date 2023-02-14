92dd2b9c7f0b4b6ef69a1b06dec1d344e3448b11
Sonu Gupta <rg3029926@gmail.com>
2023-02-14 14:39:53 +0530

    deploy json-server

---
+++ index.js
@@ -0,0 +1,10 @@
+const PORT = 5001;
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use('/sonu', router);
+server.listen(PORT, () => console.log(`JSON Server is running on port ${PORT}`));
