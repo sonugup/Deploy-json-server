59074247d47ac3a8ebb33e0b2bb687f85d1eb74f
Sonu Gupta <rg3029926@gmail.com>
2023-02-17 01:44:30 +0530

    json-server

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const PORT = 5001;
-const path = require('path');
-const jsonServer = require('json-server');
+const jsonServer = require("json-server"); 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use('/sonu', router);
-server.listen(PORT, () => console.log(`JSON Server is running on port ${PORT}`));
+server.use(router);
+
+server.listen(4500);
+
+
+
+// const PORT = 5001;
+// const path = require('path');
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router(path.join(__dirname, 'db.json'));
+// const middlewares = jsonServer.defaults();
+// server.use(middlewares);
+// server.use(jsonServer.bodyParser);
+// server.use('/sonu', router);
+// server.listen(PORT, () => console.log(`JSON Server is running on port ${PORT}`));
 
 
 
